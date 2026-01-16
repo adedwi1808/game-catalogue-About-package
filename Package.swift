e30f7256efaff90d5428f03316db292c22edf23d
Ade Dwi Prayitno <adedwip1808@gmail.com>
2026-01-17 01:28:57 +0700

    Update Core dependency to 1.0.5

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/adedwi1808/game-catalogue-Core-package.git", from: "1.0.3"),
+        .package(url: "https://github.com/adedwi1808/game-catalogue-Core-package.git", from: "1.0.5"),
         .package(
             url: "https://github.com/onevcat/Kingfisher.git",
             from: "8.6.2"
